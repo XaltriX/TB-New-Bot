18aa6aa805d50a8ddf39d935d06dc0dcd5369981
XaltriX <129171425+XaltriX@users.noreply.github.com>
2024-12-04 14:39:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "rglinks.com")
 SHORTLINK_API = os.environ.get("SHORTLINK_API", "cedfe548e9b4ac8d706ea4e23b86e13a1eaaaa9c")
 VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 57600)) # Add time in seconds
 IS_VERIFY = os.environ.get("IS_VERIFY", "True")
-TUT_VID = os.environ.get("TUT_VID", "https://t.me/HTDTeraBox/15") # shareus ka tut_vid he
+TUT_VID = os.environ.get("TUT_VID", "https://t.me/TutorialsNG/5") # shareus ka tut_vid he
